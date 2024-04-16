4eb70338499a75f485c552afd85c59479afc2bc8
Maksim <maks.severgin80@gmail.com>
2024-04-17 05:07:10 +0700

    add docstring upper and title case

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def str_upper(some_str):
+    """
+    Return a string with all uppercase
+    """
     return some_str.upper()
+
+def str_title(some_str):
+    """
+    Return 'some_str' in title case
+    """
+    return some_str.title()
